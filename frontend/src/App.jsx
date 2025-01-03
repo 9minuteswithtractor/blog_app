770de04a0e97006acc6f1a4b6ba293a4390f73dd
Peter_V <petervilson@Peters-MacBook-Air.local>
2025-01-03 14:10:52 -0800

    page layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,44 @@
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import NavBar from "./components/navbar/NavBar.component";
+import PageContainer from "./components/page-container/PageContainer.component";
 import "./App.scss";
-import "./components/navbar/NavBar.styles.scss";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <div>
+        <div className="main-layout">
           <NavBar />
           <Routes>
-            <Route path="/" element={<h3>Home</h3>} />
-            <Route path="/posts" element={<h3>Posts</h3>} />
-            <Route path="/login" element={<h3>Login</h3>} />
+            <Route
+              path="/"
+              element={
+                <PageContainer
+                  title="Welcome!"
+                  // TODO : Create content as PageComponent to display dynamic content ...
+                  content="Some info about this page ..."
+                />
+              }
+            />
+            <Route
+              path="/posts"
+              element={
+                <PageContainer
+                  title="Catch up with latest Posts!"
+                  content="Here is List of latest articles"
+                />
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <PageContainer
+                  title="Enter Your login details"
+                  content="Here is Login Form"
+                />
+              }
+            />
           </Routes>
         </div>
       </Router>
