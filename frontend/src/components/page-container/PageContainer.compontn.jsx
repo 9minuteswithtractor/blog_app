1d476a73d87b0cb3bd4d0354ed6c4b45ed81efcd
Peter_V <petervilson@Peters-MacBook-Air.local>
2025-01-03 13:20:38 -0800

    fixed Router

---
+++ frontend/src/components/page-container/PageContainer.compontn.jsx
@@ -0,0 +1,14 @@
+const PageContainer = () => {
+  return;
+
+  <main>
+    <div className="title-container">
+      <h2>Page Title</h2>
+    </div>
+    <div className="content-container">
+      <p>Page content</p>
+    </div>
+  </main>;
+};
+
+export default PageContainer;
