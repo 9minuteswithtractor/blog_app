a1d467326048248f9883a59a9601d98ab5cd6c32
Peter_V <pvilsons@icloud.com>
2025-01-04 19:37:38 -0800

    func getAllUsers

--- backend/public/index.php
+++ backend/public/index.php
@@ -5,17 +5,22 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 // CORS headers
-header('Access-Control-Allow-Origin: http://localhost:5173');
+header('Access-Control-Allow-Origin: http://localhost:5173'); // allow access from client
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 
 
-// session_start();
+
 
 // 
 
 /**
  * Checklist :
+ * 
+ *  [] - handle Login:
+ *      [] - get all user from db
+ *      [] - check if user exists
+ * 
  *  [] - frontController / router
  *  [] - Login, Posts, User controllers 
  *  [] - session 
@@ -36,11 +41,68 @@ $url = parse_url($uri, PHP_URL_PATH);
 // TODO : create func save new user
 
 
-function getAllUsers() {}
+/**
+ * Get all users from the CSV file.
+ * 
+ * @return array List of users or empty arr if no users or file doesn't exist
+ */
+
+function getAllUsers(): array
+{
+
+    // first lets check the current dir
+    echo getcwd(); // => backend/public
+    chdir('../data');
+
+    echo 'pwd after cd : ' . getcwd(); // => backend/data
+    $file = 'users.csv';
+    $filePath = getcwd() . '/' . $file;
+
+    // TODO maybe could just write filePath = "/backend/data" . $file ?
+
+
+    if (file_exists($filePath)) {
+        // echo 'Yee, file exists!';
+
+        // users arr to store local db users and to be returned for later validation 
+        $usersFromDb = [];
+
+        // open and get all user!
+        $handle = fopen($filePath, 'r'); // r for reading 
+
+        $keys = fgetcsv($handle, 0, ',', '"', '\\'); // => arr[0] for the keys ...
+
+        while (($row = fgetcsv($handle, 0, ',', '"', '\\')) !== false) {
+            $usersFromDb[] = array_combine($keys, $row);
+        }
+
+        print_r($usersFromDb);
+        return $usersFromDb;
+    } else {
+        echo 'File does not exist ...';
+        // optionally could create the users.csv file
+        // touch('users.csv');
+        return [];
+    };
+}
+
+
+
 
 
 function validateUser(string $user, string $password): bool
 {
+
+    $usersFromDB = getAllUsers();
+
+
+
+    foreach ($usersFromDB as $userFromDB) {
+
+        echo $userFromDB . '<br />';
+    }
+
+
 
 
     $validUser = 'johnny';
@@ -67,22 +129,20 @@ function registerUser(string $user, string $password): bool
     return false;
 }
 
-/**
- * 
- *  after class User
- * @return void
- */
-
 
 function login()
 {
 
 
+
+
     $rawData = file_get_contents('php://input');
     $data = json_decode($rawData, true);
 
+
     $cleanUser = htmlspecialchars($data['user']);
     $cleanPass = htmlspecialchars($data['password']);
+
 
     // validate
     $isValid = validateUser($cleanUser, $cleanPass);
@@ -93,9 +153,12 @@ function login()
         session_start();
     }
 }
+
+login();
+
+
 function register()
 {
-
 
     $rawData = file_get_contents('php://input');
     $data = json_decode($rawData, true);
@@ -116,11 +179,11 @@ function register()
 }
 
 
-if ($url === '/api/register' && $_SERVER['REQUEST_METHOD'] === 'POST') {
-    register();
-} elseif ($url === '/api/login' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+    // if ($url === '/api/register' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+    //     register();
+    // } elseif ($url === '/api/login' && $_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    login();
-} else {
-    echo 'bad request ..';
-}
+    //     login();
+    // } else {
+    //     echo 'bad request ..';
+    // }
